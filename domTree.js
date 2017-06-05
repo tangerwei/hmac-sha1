59b27dc20df4302e2792630dcfe8f79befac1e11
tangerwei <tangerwei@foxmail.com>
2017-06-05 10:08:35 +0800

    update

--- domTree.js
+++ domTree.js
@@ -1,14 +1,20 @@
-(function(w){
+(function (w) {
     //define dom node
-    var d = function(tag){
+    var d = function (tag) {
         this.node = tag;
         this.childNodes = [];
     }
     d.prototype = {
-        
+        appendChild: function (e) {
+            if (!(e instanceof DOMNode)) {
+                console.error('appendChild need a node');
+                return;
+            }
+            this.childNodes.appendChild(e);
+        }
     }
     w.DOMNode = d;
-})(window)
+})(window);
 (function (w) {
     //define dom tree
     var dt = function (o) {
